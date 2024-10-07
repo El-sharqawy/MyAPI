f46d0fb7cdfa006091957e993e001e71cbeda8d2
El-sharqawy <elsharqawy2@gmail.com>
2024-10-07 20:01:42 +0300

    My API

---
+++ users/views.py
@@ -0,0 +1,10 @@
+from .models import CustomUser
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from .serializers import UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
